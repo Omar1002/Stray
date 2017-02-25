8e12ea0077c975ca8de2c779292b5b796f4e75ac
Oliver Morrison <oliverjaymorrison@gmail.com>
2017-02-25 17:07:34 +0000

    Added light increase when next to camp fire

---
+++ Assets/Scripts/Environment/CampFireLightIncrease.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFireLightIncrease : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Player>().m_lightPool += 50;
+        }
+    }
+}
