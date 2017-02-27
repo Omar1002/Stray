777ae396fc90ad659449e46eb099960740a77da7
Oliver Morrison <oliverjaymorrison@gmail.com>
2017-02-27 22:30:46 +0000

    Added spiderweb template

---
+++ Assets/Scripts/Environment/SpiderWebScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class SpiderWebScript : MonoBehaviour {
+    public GameObject player;
+    public bool inWeb = false;
+    public float timer = 0;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (inWeb)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer >= 5.0f)
+        {
+            player.GetComponent<FirstPersonController>().m_WalkSpeed = 5.0f;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            inWeb = true;
+            player = other.gameObject;
+            other.gameObject.GetComponent<FirstPersonController>().m_WalkSpeed = 2.5f;
+        }
+    }
+}
